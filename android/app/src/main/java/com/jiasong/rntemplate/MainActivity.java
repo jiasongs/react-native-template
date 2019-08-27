72dfa9fe868f4950195f71c55b93489141bf69a8
jiasong <593908937@qq.com>
2019-08-27 19:37:51 +0800

    修改安卓的问题

--- android/app/src/main/java/com/jiasong/rntemplate/MainActivity.java
+++ android/app/src/main/java/com/jiasong/rntemplate/MainActivity.java
@@ -1,6 +1,9 @@
 package com.jiasong.rntemplate;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +15,16 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "RNTemplate";
     }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+    }
+
+
 }
