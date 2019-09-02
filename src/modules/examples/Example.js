2ef400e3a4f78281db32a0c0a2cd549d820b57ea
jiasong <593908937@qq.com>
2019-09-02 11:22:36 +0800

    no message

--- src/modules/examples/Example.js
+++ src/modules/examples/Example.js
@@ -8,38 +8,36 @@ function Example() {
   return (
     <PageContainer style={styles.container}>
       <NavigationBar title={'Example'} renderLeftAction={null} />
-      <RefreshLayout enableRefresh={false}>
-        <ScrollView>
-          <ListRow
-            title={'Theme'}
-            onPress={() => RouterHelper.navigate('DemoTheme')}
-          />
-          <ListRow
-            title={'Alert'}
-            onPress={() => RouterHelper.navigate('DemoAlert')}
-          />
-          <ListRow
-            title={'Button'}
-            onPress={() => RouterHelper.navigate('DemoButton')}
-          />
-          <ListRow
-            title={'Badge'}
-            onPress={() => RouterHelper.navigate('DemoBadge')}
-          />
-          <ListRow
-            title={'Toast'}
-            onPress={() => RouterHelper.navigate('DemoToast')}
-          />
-          <ListRow
-            title={'Overlay'}
-            onPress={() => RouterHelper.navigate('DemoOverlay')}
-          />
-          <ListRow
-            title={'List'}
-            onPress={() => RouterHelper.navigate('DemoList')}
-          />
-        </ScrollView>
-      </RefreshLayout>
+      <ScrollView refreshControl={<RefreshLayout enableRefresh={false} />}>
+        <ListRow
+          title={'Theme'}
+          onPress={() => RouterHelper.navigate('DemoTheme')}
+        />
+        <ListRow
+          title={'Alert'}
+          onPress={() => RouterHelper.navigate('DemoAlert')}
+        />
+        <ListRow
+          title={'Button'}
+          onPress={() => RouterHelper.navigate('DemoButton')}
+        />
+        <ListRow
+          title={'Badge'}
+          onPress={() => RouterHelper.navigate('DemoBadge')}
+        />
+        <ListRow
+          title={'Toast'}
+          onPress={() => RouterHelper.navigate('DemoToast')}
+        />
+        <ListRow
+          title={'Overlay'}
+          onPress={() => RouterHelper.navigate('DemoOverlay')}
+        />
+        <ListRow
+          title={'List'}
+          onPress={() => RouterHelper.navigate('DemoList')}
+        />
+      </ScrollView>
     </PageContainer>
   );
 }
