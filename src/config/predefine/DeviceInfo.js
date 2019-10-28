bbd4277ce2c58adbebafab64880e2f291a834c07
jiasong <593908937@qq.com>
2019-10-28 09:40:13 +0800

    升级依赖

--- src/config/predefine/DeviceInfo.js
+++ src/config/predefine/DeviceInfo.js
@@ -1,23 +1,6 @@
 import RNDeviceInfo from 'react-native-device-info';
-import { PixelRatio, Dimensions, Platform } from 'react-native';
+import { Platform } from 'react-native';
 
-const pixelHeight = PixelRatio.getPixelSizeForLayoutSize(
-  Dimensions.get('window').height,
-);
-
-const Device = {
-  isNotch:
-    Platform.OS === 'ios'
-      ? pixelHeight === 2436 ||
-        pixelHeight === 1792 ||
-        pixelHeight === 2688 ||
-        pixelHeight === 1624
-      : false,
+export default {
+  isNotch: Platform.OS === 'ios' ? RNDeviceInfo.hasNotch() : false,
 };
-
-// 检测是否是全民屏设备
-RNDeviceInfo.hasNotch().then((data) => {
-  Device.isNotch = data;
-});
-
-export default Device;
