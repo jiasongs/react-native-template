aad693fbf564934d56bb81e226f1552a3feacebb
jiasong <593908937@qq.com>
2020-04-14 08:36:01 +0800

    0.62

---
+++ ios/RNTemplate/Config/Header/Tesdt.mm
@@ -0,0 +1,13 @@
+//
+//  Tesdt.m
+//  RNTemplate
+//
+//  Created by jiasong on 2020/4/13.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import "Tesdt.h"
+
+@implementation Tesdt
+
+@end
